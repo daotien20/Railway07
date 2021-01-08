143eeb90437aa3d9925c9ca1a89bff1f3614ab22
daotien20 <daotien20120@gmail.com>
2021-01-08 10:43:18 +0700

    submit assignment 2

---
+++ MySQL/Testing_System_Assigment_2.sql
@@ -0,0 +1,99 @@
+DROP DATABASE IF EXISTS Testing_System_Assignment_1;
+CREATE DATABASE Testing_System_Assignment_1;
+USE Testing_System_Assignment_1;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE IF NOT EXISTS department (
+	department_id	TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	department_name	NVARCHAR(50) UNIQUE KEY NOT NULL
+);
+
+DROP TABLE IF EXISTS position;
+CREATE TABLE IF NOT EXISTS position (
+	position_id		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	position_name	ENUM('Dev', 'Test', 'Scum Master', 'PM')
+);
+
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE IF NOT EXISTS `account` (
+	account_id		SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	email			VARCHAR(100) UNIQUE KEY NOT NULL,
+	user_name		VARCHAR(50) UNIQUE KEY NOT NULL,
+	full_name		NVARCHAR(50),
+	department_id	TINYINT UNSIGNED,
+	position_id		TINYINT UNSIGNED,
+	creat_date		DATE,
+    FOREIGN KEY (department_id)	REFERENCES department (department_id),
+    FOREIGN KEY (position_id)	REFERENCES position (position_id)
+);
+
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE IF NOT EXISTS `group` (
+	group_id		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	group_name		VARCHAR(200),
+	creator_id		SMALLINT UNSIGNED,
+	creat_date		DATE,
+	FOREIGN KEY (creator_id) REFERENCES `account` (account_id) 
+);
+
+DROP TABLE IF EXISTS group_account;
+CREATE TABLE IF NOT EXISTS group_account (
+	group_id		TINYINT UNSIGNED,
+	account_id		SMALLINT UNSIGNED,
+	join_date		DATE,
+	PRIMARY KEY (group_id,account_id)
+);
+
+DROP TABLE IF EXISTS type_question;
+CREATE TABLE IF NOT EXISTS type_question (
+	type_id			SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	type_name		ENUM('Essay', 'Multiple-Choice')
+);
+
+DROP TABLE IF EXISTS category_question;
+CREATE TABLE IF NOT EXISTS category_question (
+	category_id		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	category_name	VARCHAR(200)
+);
+
+DROP TABLE IF EXISTS question;
+CREATE TABLE IF NOT EXISTS question (
+	question_id		SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	content			TEXT,
+	category_id		TINYINT UNSIGNED,
+	type_id			SMALLINT UNSIGNED,
+	creator_id		SMALLINT UNSIGNED,
+	creat_date		DATE,
+	FOREIGN KEY (category_id)	REFERENCES category_question(category_id),
+	FOREIGN KEY (type_id)		REFERENCES type_question (type_id),
+	FOREIGN KEY (creator_id) REFERENCES `account` (account_id) 
+);
+
+DROP TABLE IF EXISTS answer;
+CREATE TABLE IF NOT EXISTS answer (
+	answer_id		SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	content			TEXT,
+	question_id		SMALLINT UNSIGNED,
+	is_correct		ENUM('True', 'False'),
+	FOREIGN KEY (question_id)	REFERENCES question (question_id)
+);
+
+DROP TABLE IF EXISTS exam;
+CREATE TABLE IF NOT EXISTS exam (
+	exam_id			SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`code`			VARCHAR(50) UNIQUE KEY NOT NULL,
+	title			VARCHAR(100),
+	category_id		TINYINT UNSIGNED,
+	duration		TINYINT UNSIGNED,
+	creator_id		SMALLINT UNSIGNED,
+	creat_date		DATE,
+	FOREIGN KEY (category_id)	REFERENCES category_question (category_id),
+	FOREIGN KEY (creator_id) 	REFERENCES `account` (account_id) 
+);
+
+DROP TABLE IF EXISTS exam_question;
+CREATE TABLE IF NOT EXISTS exam_question (
+	exam_id			SMALLINT UNSIGNED,
+	question_id		SMALLINT UNSIGNED,
+    PRIMARY KEY (exam_id,question_id)
+);
